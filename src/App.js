2240fc7ade836ae9b98eea95f82c4cb26e8f2991
Niimaii <nimaicgomes@gmail.com>
2023-01-28 01:27:44 -0300

    Shared Layouts

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Products from "./pages/Products";
+import Error from "./pages/Error";
 
 function App() {
   return (
     <BrowserRouter>
+      {/* Anything put above or bellow the Routes will show up on all the pages */}
+      <nav>Navbar</nav>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/products" element={<Products />} />
+        <Route path="/" element={<Home />}>
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          {/* This will catch any url request that doesn't match out routes */}
+          <Route path="*" element={<Error />} />
+        </Route>
       </Routes>
+      <footer>Footer</footer>
     </BrowserRouter>
   );
 }
